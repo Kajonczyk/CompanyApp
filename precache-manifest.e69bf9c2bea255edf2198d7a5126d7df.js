8f2ac5123a6938be3d4b108db64189e7a2a6ef94
Kajonczyk <kopczackikamil2000@gmail.com>
2019-09-05 21:10:45 +0200

    Updates

---
+++ precache-manifest.e69bf9c2bea255edf2198d7a5126d7df.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a22830dc1fa145b87b1842956f8bf94",
+    "url": "/CompanyApp/index.html"
+  },
+  {
+    "revision": "6c3fe8bb7dc14bc9a492",
+    "url": "/CompanyApp/static/js/2.15ba6cc6.chunk.js"
+  },
+  {
+    "revision": "a312296849678a6cc3cb",
+    "url": "/CompanyApp/static/js/main.48136dcc.chunk.js"
+  },
+  {
+    "revision": "a05a410df6c40f0d3d32",
+    "url": "/CompanyApp/static/js/runtime~main.9951421a.js"
+  }
+]);
